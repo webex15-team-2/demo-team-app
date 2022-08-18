731817a850b9119c076c084418ca73a860da14a9
RyosukeNagae <r.nagae.944@nitech.jp>
2022-08-18 15:43:14 +0900

    memo(保存機能なし)

--- src/views/MemoApp.vue
+++ src/views/MemoApp.vue
@@ -2,37 +2,52 @@
   <h1>Vue メモ</h1>
   <div class="memo-list">
     <ul class="memo-list__container">
-      <li class="memo">
+      <li v-for="(memo, index) in memos" :key="index" class="memo">
         <div class="memo__checkbox">
-          <input type="checkbox" />
+          <input type="checkbox" v-model="memo.isDone" />
         </div>
-        <div class="memo__text">ひき肉を300g買う</div>
-        <button class="memo__delete">削除</button>
-      </li>
-      <li class="memo">
-        <div class="memo__checkbox">
-          <input type="checkbox" />
+        <div v-if="memo.isDone" class="memo__text memo__text--done">
+          {{ index }}:{{ memo.text }}
         </div>
-        <div class="memo__text">ホウレンソウを1束買う</div>
-        <button class="memo__delete">削除</button>
-      </li>
-      <li class="memo">
-        <div class="memo__checkbox">
-          <input type="checkbox" />
-        </div>
-        <div class="memo__text">ピーマンを2個買う</div>
-        <button class="memo__delete">削除</button>
+        <div v-else class="memo__text">{{ index }}:{{ memo.text }}</div>
+        <button @click="deleteMemo(index)">削除</button>
+        <!-- <div class="spin">
+          <button class="spin-minus">↓</button>
+          <input class="spin-input" type="number" min="0" max="100" step="1" />
+          <button class="spin-plus">↑</button>
+        </div> -->
       </li>
     </ul>
     <div class="add-memo-field">
-      <input class="add-memo-field__input" type="text" />
-      <button class="add-memo-field__button">追加</button>
+      <input class="add-memo-field__input" type="text" v-model="inputMemo" />
+      <button @click="addMemo" class="add-memo-field__button">追加</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      inputMemo: "",
+      memos: [],
+    }
+  },
+
+  methods: {
+    addMemo() {
+      if (this.inputMemo !== "") {
+        const memo = { text: this.inputMemo, isDone: false }
+        this.memos.push(memo)
+        this.inputMemo = ""
+      }
+    },
+
+    deleteMemo(index) {
+      this.memos.splice(index, 1)
+    },
+  },
+}
 </script>
 
 <style scoped>
@@ -69,6 +84,7 @@ export default {}
   text-align: left;
 }
 
+/* 打ち消し線 */
 .memo__text--done {
   text-decoration-line: line-through;
 }
